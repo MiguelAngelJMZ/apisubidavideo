3ce7a0a801f0c197bc5f048b87d8238c2c2a3e71
MiguelAngelJMZ <miguel.angel.ljmz@gmail.com>
2023-06-22 23:09:59 -0600

    initial commit

--- deploy_API.sh
+++ deploy_API.sh
@@ -29,7 +29,7 @@ deploy_API()
       --source=${code_source} \
       --trigger-http \
       --entry-point=$1 \
-      --vpc_connector=${vpc_connector}
+      --vpc-connector=${vpc_connector}
 }
 
 echo "Validating Finished"
